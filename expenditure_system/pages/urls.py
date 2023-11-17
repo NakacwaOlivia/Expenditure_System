04ca8ea71457bf290a323e5c568af81e427ada37
Kkarisha <muwongeshanitak@gmail.com>
2023-11-17 20:39:16 +0300

    changes by shanita

--- expenditure_system/pages/urls.py
+++ expenditure_system/pages/urls.py
@@ -1,6 +1,26 @@
 from django.urls import path
 from .views import WelcomePageView, UpdatePageView, RegisterPageView, ProfilePageView, LoginPageView, DashboardPageView, ConfirmPageView
+# urls.py
 
+#_____________________edited this to new one_______________-
+from django.urls import path    
+from . import views
+
+
+urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('confirm/', views.confirm_email, name='confirm_page'),  # Use 'confirm_page' as the name
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('welcome/', views.welcome, name='welcome_page'),  # Define a named URL pattern for the welcome page
+    # Other URL patterns...
+]
+# _______________________ends here_____________________________
+
+
+
+
+
+# ________________________original code_________________________________________
 urlpatterns = [
     path('', WelcomePageView.as_view(), name='welcome'),
     path('update/', UpdatePageView.as_view(), name='update'),
@@ -9,5 +29,8 @@ urlpatterns = [
     path('login/', LoginPageView.as_view(), name='login'),
     path('dashboard/', DashboardPageView.as_view(), name='dashboard'),
     path('confirm/', ConfirmPageView.as_view(), name='confirm'),
+
+
+    
 
 ]
